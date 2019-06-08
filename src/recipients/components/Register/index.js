a4776ca3b3181bb608b6f96ecc87bc40345e4c57
Elliott J. Humphreys <elliotthumphreys821@gmail.com>
2019-06-08 18:02:23 +0100

    added shared thin header

--- src/recipients/components/Register/index.js
+++ src/recipients/components/Register/index.js
@@ -1,50 +1,32 @@
 import React, { useState, useContext } from 'react'
-import styled from 'styled-components'
+import styles from 'styled-components'
+import { ArrowBack, Done } from '@material-ui/icons'
+import { ThinHeader } from '../../../shared-components/Header'
 
-const Background = styled.div`
-    background: #ff6b6b;
-    color: #222f3e;
+
+const Container = styles.div`
     display: flex;
-    justify-content: center;
-    flex-flow: column wrap;
+    justify-content: space-between;
+    color: white;
+    height: 100%;
+    padding: 0 0.5em;
+    > a {
+        height: 100%;
+        > svg {
+            height: 100%;
+        }
+    }
 `
 
-const CoverImage = styled.img`
-    border: 3px solid #ccc;
-    border-radius: 999px;
-    height: 5rem;
-    width: 5rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
+const Register = () => {
+    const renderBody = () => (
+        <Container>
+            <a><ArrowBack/></a>
+            <a><Done/></a>
+        </Container>
+    )
 
-const GenericBtn = styled.button`
-    border-radius: 999px;
-    border: 3px solid #ccc;
-    height: 2rem;
-    margin: 1rem;
-    background: #fff;
-`
+    return <ThinHeader body={renderBody} />
+}
 
-const PotContainer = styled.div`
-    display: flex;
-    flex-flow: column wrap;
-    justify-content: center;
-    align-items: space-around;
-
-`
-
-const Start = () => <Background>
-        <CoverImage src='./images/maxresdefault.jpg' />
-        <h2>Some guy</h2>
-        <h3>age</h3>
-        <PotContainer>
-            <GenericBtn>Soup</GenericBtn>
-            <GenericBtn>Shelter</GenericBtn>
-            <GenericBtn>Suit</GenericBtn>
-        </PotContainer>
-        <GenericBtn className="donate-btn">DONATE</GenericBtn>
-    </Background>
-
-export default Start
+export default Register
--- src/shared-components/Header/index.js
+++ src/shared-components/Header/index.js
@@ -2,4 +2,10 @@ import React from 'react'
 
 const Header = () => <h1 className='header-text'>PotPay<span>&trade;</span></h1>
 
+export const ThinHeader = ({body}) => (
+    <header className='thin-header'>
+        {body() || Header()}
+    </header>
+)
+
 export default Header
