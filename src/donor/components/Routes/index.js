fc61bcc7580d8b61fdf4b738a846a4f412f3cc3e
Kyle Watson <kylewatson98@gmail.com>
2019-06-08 13:11:43 +0100

    added json loader

--- src/donor/components/Routes/index.js
+++ src/donor/components/Routes/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Route, Switch } from 'react-router-dom';
 import Start from "../Start"
+import RecipientBio from "../RecipientBio"
 import { Provider } from '../Context'
 
 import "../../sass/main.scss"
@@ -10,6 +11,7 @@ const Routes = () => {
     <Provider>
         <Switch>
           <Route path="/start" component={Start} />
+          <Route path="/recipient-bio" component={RecipientBio} />
         </Switch>
     </Provider>
   )
