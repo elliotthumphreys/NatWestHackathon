900beaba3969441637badab7494122b129813cb3
Elliott J. Humphreys <elliotthumphreys821@gmail.com>
2019-06-09 13:28:04 +0100

    changes

--- src/recipients/components/Edit/index.js
+++ src/recipients/components/Edit/index.js
@@ -5,18 +5,39 @@ import Input from '../../../shared-components/Input'
 import Button from '../../../shared-components/Button'
 import SelectInput from '../SelectInput'
 import { Delete } from '@material-ui/icons'
+import styles from 'styled-components'
 
 const Edit = ({ match: { params: { id } } }) => {
     const [persons, setPersons] = useContext(Context)
 
     const [pots, setPots] = useState(persons.find(_ => _.id === id).pots)
 
+    const [showMessage, setShowMessage] = useState(false)
+
+    const [failedDeletedMessage, setFailedDeletedMessage] = useState('')
+
     const deletePot = id => {
-        setPots(pots.filter(_ => _.id !== id))
+        setShowMessage(false)
+        const pot = pots.find(_ => _.id === id)
+        if(pot.amount > 0){
+            setShowMessage(true)
+            setFailedDeletedMessage(`The pot ${pot.name} can not be deleted due to the balance of £${pot.amount}`)
+        }else{
+            setPots(pots.filter(_ => _.id !== id))
+        }
     }
+
+    const Message = styles.p`
+        color: red;
+        width: 80%;
+        font-size: 0.9em;
+        margin: 0 auto;
+        padding: 0.5em;
+    `
 
     return (
         <AdminHeader person={persons.find(_ => _.id === id)}>
+            {showMessage && <Message>{failedDeletedMessage}</Message> }
             {
                 pots.map(_ => (
                     <a onClick={() => deletePot(_.id)}>
--- src/recipients/components/Stats/index.js
+++ src/recipients/components/Stats/index.js
@@ -2,13 +2,61 @@ import React, { useContext } from 'react'
 import AdminHeader from '../AdminHeader'
 import { Context } from '../../../Context'
 import Input from '../../../shared-components/Input'
+import Interaction from 'react-minimal-pie-chart'
+import { style } from '@material-ui/system';
+import styles from 'styled-components'
+
+
+const colors = ['#F1C47B', '#F4DC7B', '#5FC2D1', '#f67979']
+
+const Section = styles.section`
+    width: 80%;
+    margin: 0 auto;
+`
+
+const Div = styles.div`
+    margin-top: 1em
+p {
+    text-align: left;
+}    
+span: {
+        width: 10px;
+        height: 10px;
+    }
+`
 
 const Stats = ({ match: { params: { id } } }) => {
     const [persons, setPersons] = useContext(Context)
+    const pots = persons.find(_ => _.id === id).pots
+
+    const data = pots.map((pot, index) => {
+        return {
+            title: pot.name,
+            value: pot.amount,
+            color: colors[index % 4]
+        }
+    })
+
+    const getKeys = pots.map((pot, index) => {
+        const Span = styles.span`
+                background-color: ${colors[index % 4]}
+                display: inline-block;
+                padding: 0.5em;
+                margin-right: 1em;
+                border-radius: 50%;
+        `
+
+        return <Div>
+            <p><Span />{pot.name}</p>
+        </Div>
+    })
 
     return (
         <AdminHeader person={persons.find(_ => _.id === id)}>
-
+            <Section>
+                <Interaction data={data} />
+                {getKeys}
+            </Section>
         </AdminHeader>
     )
 }
